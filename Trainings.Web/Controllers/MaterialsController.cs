aceebca67d4363e1ee7ae62242093b6f1d594d2e
sergeymilashico <sergey@milashico.com>
2022-04-17 21:08:22 -0500

    add sitemap

--- Trainings.Web/Controllers/MaterialsController.cs
+++ Trainings.Web/Controllers/MaterialsController.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using Trainings.Web.Models;
 using Trainings.Web.Services;
+using X.Web.Sitemap;
 
 namespace Trainings.Web.Controllers
 {
@@ -24,5 +25,49 @@ namespace Trainings.Web.Controllers
             return View();
         }
 
+        public ContentResult SiteMap()
+        {
+            var sitemap = new Sitemap();
+            string siteUrl = "https://milashico.com/";
+
+            sitemap.Add(new Url
+            {
+                ChangeFrequency = ChangeFrequency.Daily,
+                Location = siteUrl,
+                Priority = 0.5,
+                TimeStamp = DateTime.Now
+            });
+
+            var lessonService = new LessonService();
+
+            sitemap.Add(CreateUrl($"{siteUrl}/Home/Index"));
+            sitemap.Add(CreateUrl($"{siteUrl}Home/Schedule"));
+            sitemap.Add(CreateUrl($"{siteUrl}Home/Courses"));
+            sitemap.Add(CreateUrl($"{siteUrl}Home/About"));
+            sitemap.Add(CreateUrl($"{siteUrl}Home/Lessons"));
+            var lessons = lessonService.GetAllLessons();
+            foreach(var lesson in lessons)
+            {
+                sitemap.Add(CreateUrl($"{siteUrl}Home/Lesson/{lesson.Id}"));
+            }
+
+            return new ContentResult
+            {
+                Content = sitemap.ToXml(),
+                ContentType = "application/xml",
+                StatusCode = 200
+            };
+        }
+
+        private static Url CreateUrl(string url)
+        {
+            return new Url
+            {
+                ChangeFrequency = ChangeFrequency.Daily,
+                Location = url,
+                Priority = 0.5,
+                TimeStamp = DateTime.Now
+            };
+        }
     }
 }
